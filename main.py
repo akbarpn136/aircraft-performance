0b6e9212c55d68bde7b978670546d53b5d8ca599
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-09-17 18:13:47 +0700

    otomasi perhitungan kondisi trim CL dan CD terhadap run number pengujian

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from dask import (dataframe as dd, array as da)
 
 
-def load_data(filename):
+def load_data(projectname):
     """Fungsi untuk baca dan olah data"""
 
-    kolom = ["V0", "Q0", "ALPHA", "BETA", "CL", "CD", "CM25", "CY", "CYAW", "CROLL"]
+    kolom = ["RUN", "V0", "Q0", "ALPHA", "BETA", "CL", "CD", "CM25", "CY", "CYAW", "CROLL"]
 
-    df = dd.read_csv(f"data/{filename}", sep="\t", header=None, names=kolom)
-    df = df[["ALPHA", "CL", "CD", "CM25"]]
+    df = dd.read_csv(f"data/{projectname}/run*.csv", sep="\t", header=None, names=kolom)
+    df = df[["RUN", "ALPHA", "CL", "CD", "CM25"]]
 
     # CLCG20 CLCG25 CLCG30 CLCG35 merupakan variabel untuk variasi CM terhadap CG masing-masing 20%, 25%, 30% dan 35%
     # dalam perhitungan CL trim
     df["CLCG20"] = df["CM25"] + (0.20 - 0.25) * df["CL"]
+    df["CLCG20_TRIM"] = df["CL"].diff(-1).fillna(0) / df["CLCG20"].diff(-1).fillna(0) * -1 * df["CLCG20"] + df["CL"]
+
     df["CLCG25"] = df["CM25"] + (0.25 - 0.25) * df["CL"]
+    df["CLCG25_TRIM"] = df["CL"].diff(-1).fillna(0) / df["CLCG25"].diff(-1).fillna(0) * -1 * df["CLCG25"] + df["CL"]
+
     df["CLCG30"] = df["CM25"] + (0.30 - 0.25) * df["CL"]
+    df["CLCG30_TRIM"] = df["CL"].diff(-1).fillna(0) / df["CLCG30"].diff(-1).fillna(0) * -1 * df["CLCG30"] + df["CL"]
+
     df["CLCG35"] = df["CM25"] + (0.35 - 0.25) * df["CL"]
+    df["CLCG35_TRIM"] = df["CL"].diff(-1).fillna(0) / df["CLCG35"].diff(-1).fillna(0) * -1 * df["CLCG35"] + df["CL"]
 
     # CDCG20 CDCG25 CDCG30 CDCG35 merupakan variabel untuk variasi CM terhadap CG masing-masing 20%, 25%, 30% dan 35%
     # dalam perhitungan CD trim
@@ -23,57 +30,28 @@ def load_data(filename):
     df["CDCG30"] = df["CM25"] + (0.30 - 0.25) * df["CD"]
     df["CDCG35"] = df["CM25"] + (0.35 - 0.25) * df["CD"]
 
-    return df
+    return df.drop(["ALPHA", "CL", "CD", "CM25"], axis=1)
 
 
-def _find_trim_files(row):
-    """
-    Fungsi untuk mencari index saat terjadi kondisi trim per run pengujian. Fungsi ini juga bersifat lazy sehingga untuk
-    mendapatkan hasil perhitungan harus memanggil perintah compute()
-    """
+def get_trim_data(df, column_name):
+    sign = df[column_name].map(da.sign)
+    diff_positive = sign.diff(periods=-1).fillna(0)
+    df_positive = df.loc[diff_positive[diff_positive != 0].index]
+    df_positive = df_positive[["RUN", f"{column_name}_TRIM"]].head(1)
 
-    # Index dari dataframe df (CL trim) dimana CM bernilai negatif disimpan kedalam Array
-    trim_idx_cl_20 = row[row["CLCG20"] < 0].head(1).index.values
-    trim_idx_cl_25 = row[row["CLCG25"] < 0].head(1).index.values
-    trim_idx_cl_30 = row[row["CLCG30"] < 0].head(1).index.values
-    trim_idx_cl_35 = row[row["CLCG35"] < 0].head(1).index.values
-
-    # Index dari dataframe df (CD trim) dimana CM bernilai negatif disimpan kedalam Array
-    trim_idx_cd_20 = row[row["CDCG20"] < 0].head(1).index.values
-    trim_idx_cd_25 = row[row["CDCG25"] < 0].head(1).index.values
-    trim_idx_cd_30 = row[row["CDCG30"] < 0].head(1).index.values
-    trim_idx_cd_35 = row[row["CDCG35"] < 0].head(1).index.values
-
-    return da.concatenate([
-        trim_idx_cl_20,
-        trim_idx_cl_25,
-        trim_idx_cl_30,
-        trim_idx_cl_35,
-        trim_idx_cd_20,
-        trim_idx_cd_25,
-        trim_idx_cd_30,
-        trim_idx_cd_35,
-    ])
-
-
-def get_trim_index(df):
-    """Fungsi ini digunakan untuk perhitungan kondisi trim untuk CL dan CD"""
-
-    df = df.map_partitions(_find_trim_files)
-
-    # indexes dalam bentuk array dengan jumlah sebanyak 8 kolom saat mulai trim
-    # clcg20, clcg25, clcg30, clcg35, cdcg20, cdcg25, cdcg30, cdcg35
-    # banyaknya baris bergantung banyaknya file windtunnel (jumlah run number sehingga menggunakan -1 untuk reshape)
-    # dan memenuhi kondisi trim
-    return df.compute_chunk_sizes().compute().compute().reshape(-1, 8)
+    return df_positive
 
 
 def process():
-    df = load_data("run*.csv")
-    indexes = get_trim_index(df)
+    df = load_data("male")
 
-    print(indexes)
-    print(indexes[:, 0])
+    clcg20_trim = df.map_partitions(get_trim_data, "CLCG20")
+    clcg25_trim = df.map_partitions(get_trim_data, "CLCG25")
+    clcg30_trim = df.map_partitions(get_trim_data, "CLCG30")
+    clcg35_trim = df.map_partitions(get_trim_data, "CLCG35")
+
+    trim = clcg20_trim.merge(clcg25_trim).merge(clcg30_trim).merge(clcg35_trim)
+    print(trim.compute())
 
 
 if __name__ == "__main__":
