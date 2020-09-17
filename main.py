d3c94906c3e51b44168f137a8d1cc20a78645eda
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-09-17 11:08:50 +0700

    modifikasi fungsi untuk index trim

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
 from dask import (dataframe as dd, array as da)
 
 
-def search_multiple_index(df):
-    """Fungsi ini digunakan sebagai fungsi bantuan dalam pencarian index saat terjadi trim untuk masing-masing data
-    windtunnel (jumlah run number)"""
-
-    # temukan urutan (0 based) dari index saat trim untuk masing-masing file windtunnel (index < 0)
-    trim_idx = da.diff(df).argmin() + 1  # karena 0 based jadi perlu ditambahkan 1
-
-    # ambil nilai sebenarnya berdasarkan index yang terpilih
-    trim_idx = da.take(df, da.concatenate([[0], [trim_idx]]))
-
-    return trim_idx
-
-
 def load_data(filename):
     """Fungsi untuk baca dan olah data"""
 
@@ -39,38 +26,25 @@ def load_data(filename):
     return df
 
 
-def get_trim_index(df):
+def _find_trim_files(row):
     """
-    Fungsi untuk mencari index saat terjadi kondisi trim. Fungsi ini juga bersifat lazy sehingga untuk mendapatkan hasil
-    perhitungan harus memanggil perintah compute()
+    Fungsi untuk mencari index saat terjadi kondisi trim per run pengujian. Fungsi ini juga bersifat lazy sehingga untuk
+    mendapatkan hasil perhitungan harus memanggil perintah compute()
     """
 
     # Index dari dataframe df (CL trim) dimana CM bernilai negatif disimpan kedalam Array
-    clcg20 = df[df["CLCG20"] < 0].iloc[:, 0].index.to_dask_array(lengths=True)
-    clcg25 = df[df["CLCG25"] < 0].iloc[:, 0].index.to_dask_array(lengths=True)
-    clcg30 = df[df["CLCG30"] < 0].iloc[:, 0].index.to_dask_array(lengths=True)
-    clcg35 = df[df["CLCG35"] < 0].iloc[:, 0].index.to_dask_array(lengths=True)
+    trim_idx_cl_20 = row[row["CLCG20"] < 0].head(1).index.values
+    trim_idx_cl_25 = row[row["CLCG25"] < 0].head(1).index.values
+    trim_idx_cl_30 = row[row["CLCG30"] < 0].head(1).index.values
+    trim_idx_cl_35 = row[row["CLCG35"] < 0].head(1).index.values
 
     # Index dari dataframe df (CD trim) dimana CM bernilai negatif disimpan kedalam Array
-    cdcg20 = df[df["CDCG20"] < 0].iloc[:, 0].index.to_dask_array(lengths=True)
-    cdcg25 = df[df["CDCG25"] < 0].iloc[:, 0].index.to_dask_array(lengths=True)
-    cdcg30 = df[df["CDCG30"] < 0].iloc[:, 0].index.to_dask_array(lengths=True)
-    cdcg35 = df[df["CDCG35"] < 0].iloc[:, 0].index.to_dask_array(lengths=True)
+    trim_idx_cd_20 = row[row["CDCG20"] < 0].head(1).index.values
+    trim_idx_cd_25 = row[row["CDCG25"] < 0].head(1).index.values
+    trim_idx_cd_30 = row[row["CDCG30"] < 0].head(1).index.values
+    trim_idx_cd_35 = row[row["CDCG35"] < 0].head(1).index.values
 
-    # indexes dalam bentuk array dengan jumlah sebanyak 8 kolom saat mulai trim
-    # clcg20, clcg25, clcg30, clcg35, cdcg20, cdcg25, cdcg30, cdcg35
-    # banyaknya baris bergantung banyaknya file windtunnel (jumlah run number) dan memenuhi kondisi trim
-    trim_idx_cl_20 = search_multiple_index(clcg20).reshape(2, 1)
-    trim_idx_cl_25 = search_multiple_index(clcg25).reshape(2, 1)
-    trim_idx_cl_30 = search_multiple_index(clcg30).reshape(2, 1)
-    trim_idx_cl_35 = search_multiple_index(clcg35).reshape(2, 1)
-    trim_idx_cd_20 = search_multiple_index(cdcg20).reshape(2, 1)
-    trim_idx_cd_25 = search_multiple_index(cdcg25).reshape(2, 1)
-    trim_idx_cd_30 = search_multiple_index(cdcg30).reshape(2, 1)
-    trim_idx_cd_35 = search_multiple_index(cdcg35).reshape(2, 1)
-
-    # Kombinasi keseluruhan index trim
-    comb_trim = da.concatenate([
+    return da.concatenate([
         trim_idx_cl_20,
         trim_idx_cl_25,
         trim_idx_cl_30,
@@ -79,16 +53,27 @@ def get_trim_index(df):
         trim_idx_cd_25,
         trim_idx_cd_30,
         trim_idx_cd_35,
-    ], axis=1)
+    ])
 
-    return comb_trim
+
+def get_trim_index(df):
+    """Fungsi ini digunakan untuk perhitungan kondisi trim untuk CL dan CD"""
+
+    df = df.map_partitions(_find_trim_files)
+
+    # indexes dalam bentuk array dengan jumlah sebanyak 8 kolom saat mulai trim
+    # clcg20, clcg25, clcg30, clcg35, cdcg20, cdcg25, cdcg30, cdcg35
+    # banyaknya baris bergantung banyaknya file windtunnel (jumlah run number sehingga menggunakan -1 untuk reshape)
+    # dan memenuhi kondisi trim
+    return df.compute_chunk_sizes().compute().compute().reshape(-1, 8)
 
 
 def process():
     df = load_data("run*.csv")
     indexes = get_trim_index(df)
 
-    print(indexes.compute())
+    print(indexes)
+    print(indexes[:, 0])
 
 
 if __name__ == "__main__":
